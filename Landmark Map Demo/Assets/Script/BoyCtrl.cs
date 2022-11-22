0a6bf1c72a0de8d8fe963f29ee08c8f07330b0c3
Claire <clairism07@gmail.com>
2022-11-22 15:58:58 -0800

    Initial commit - Unity project upload
    Landmark Map Demo by Yuyi. Unity version - 2020.3.41

---
+++ Landmark Map Demo/Assets/Script/BoyCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyCtrl : MonoBehaviour
+{
+
+    private Animator ator;
+    
+
+    private MoveAvatar moveAvatar;
+    
+
+	void Start ()
+	{
+	    ator = gameObject.GetComponent<Animator>();
+        
+
+	    moveAvatar = transform.parent.GetComponent<MoveAvatar>();
+        
+	}
+	
+	void Update () {
+	    if (moveAvatar.animationState==MoveAvatar.AvatarAnimationState.Idle)
+            
+	    {
+	        if (!ator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                
+	        {
+	            ator.SetTrigger("Idle");
+            }
+	            
+	    }else if (moveAvatar.animationState == MoveAvatar.AvatarAnimationState.Walk)
+	    {
+	        if (!ator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+	        {
+	            ator.SetTrigger("Walk");
+            }        
+        }
+	    else if (moveAvatar.animationState == MoveAvatar.AvatarAnimationState.Run)
+	    {
+	        if (!ator.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+	        {
+	            ator.SetTrigger("Run");
+            }
+	        
+	    }
+    }
+}
