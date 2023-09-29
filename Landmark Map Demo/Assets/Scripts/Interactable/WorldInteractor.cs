e0906e54f97d8492c21ad7c71e3efefdc3f71815
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-09-29 10:12:36 -0700

    Locked interactions
    Player can no longer interact with anything else while they are in an interaction
    Press esc to exit interaction for now

--- Landmark Map Demo/Assets/Scripts/Interactable/WorldInteractor.cs
+++ Landmark Map Demo/Assets/Scripts/Interactable/WorldInteractor.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class WorldInteractor : MonoBehaviour
 {
+    [Header("Interaction System")]
+    [SerializeField] private GameObject interactingIndicator;
+    [SerializeField] private LayerMask interactable;
+    [SerializeField] private KeyCode clearInteractKey = KeyCode.Escape;
+
+    [Header("References")]
     [SerializeField] private CoinInventory coinInventory;
 
-    [SerializeField] private LayerMask interactable;
-
     private InteractableObject _currentInteractableObject;
+    private bool _inInteraction;
 
     void Update()
     {
+        if (Input.GetKeyDown(clearInteractKey))
+            ClearInteraction();
+                
         if (Input.GetKeyUp(KeyCode.Mouse0))
             OnClick();
     }
 
+    // Clears the current interactable object
+    private void ClearInteraction()
+    {
+        _currentInteractableObject.Deselect();
+        _currentInteractableObject = null;
+
+        _inInteraction = false;
+        interactingIndicator.SetActive(false);
+    }
+
+    // What heppens when the player clicks on the screen
     private void OnClick()
     {
+        if (_inInteraction)
+            return; // TODO: Put code for what happens durring interactions here
+
+        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit, 500, interactable))
         {
@@ -26,15 +49,15 @@ public class WorldInteractor : MonoBehaviour
         }
     }
 
+    // Tries to interact with an interactable object
     private void TryObjectInteraction(RaycastHit hit)
     {
         if (hit.transform.CompareTag("Interactable Object"))
         {
-            if (_currentInteractableObject != null)
-                _currentInteractableObject.Deselect();
+            interactingIndicator.SetActive(true);
 
+            _inInteraction = true;
             _currentInteractableObject = hit.transform.GetComponent<InteractableObject>();
-
             _currentInteractableObject.Select();
         }
     }
