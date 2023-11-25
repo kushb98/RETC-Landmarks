e16d61aa6e8bc7de4cb306dddffe1ac56b583e90
Martin Allsbrook <maal5844@colorado.edu>
2023-11-25 11:54:24 -0500

    Added documentation in the form of comments

--- Landmarks Lightship/Assets/Scripts/Camera/CameraController.cs
+++ Landmarks Lightship/Assets/Scripts/Camera/CameraController.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The CameraController class is responsible for managing the active / inactive state of lightships's orbit camera, and a second focus camera.
+/// It provides methods to focus on a specific target by enabling the focus camera and disabling the lightship camera, and vice versa.
+/// </summary>
 public class CameraController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject lightshipOrbitCamera;
     [SerializeField] private CameraFocusedState cameraFocusedState;
 
+    /// <summary>
+    /// Focuses on a specific target by disabling the lightshipOrbitCamera and enabling the cameraFocusedState (which holds the focus camera).
+    /// </summary>
+    /// <param name="target">The Transform object to focus on.</param>
     public void FocusOn(Transform target)
     {
-        if (lightshipOrbitCamera.activeInHierarchy) 
+        if (lightshipOrbitCamera.activeInHierarchy)
             lightshipOrbitCamera.SetActive(false);
 
         if (!cameraFocusedState.gameObject.activeInHierarchy)
@@ -19,6 +27,9 @@ public class CameraController : MonoBehaviour
         cameraFocusedState.SetTarget(target);
     }
 
+    /// <summary>
+    /// Breaks the focus by enabling the lightshipOrbitCamera and disabling the cameraFocusedState.
+    /// </summary>
     public void BreakFocus()
     {
         if (!lightshipOrbitCamera.activeInHierarchy)
--- Landmarks Lightship/Assets/Scripts/Interactable/InteractableObject.cs
+++ Landmarks Lightship/Assets/Scripts/Interactable/InteractableObject.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// The basis for any interactable objects in the game
-/// Provides basic functionality and allows for integration with WorldInteractor
+/// The InteractableObject class is the base class for all interactable objects in the game.
+/// It provides basic functionality for interaction and can be extended to create more complex interactable objects.
+/// 
+/// To create a new type of interactable object, extend this class and override the necessary methods.
+/// The WorldInteractor class interacts with objects of this type, and by overriding specific methods,
+/// you can customize how the WorldInteractor interacts with your new class.
+/// 
+/// The following methods can be overridden:
+/// - ReadyForInteraction: Determines whether the object is ready for interaction.
+/// - Interact: Defines the interaction behavior of the object.
+/// - Consume: Defines the behavior when the object is consumed.
+/// - MakeReady: Defines the behavior when the object is made ready for interaction.
+/// 
+/// Note: If an object is selected, it will automatically be deselected when it is destroyed.
 /// </summary>
 public class InteractableObject : MonoBehaviour
 {
@@ -15,17 +27,6 @@ public class InteractableObject : MonoBehaviour
     private WorldInteractor _worldInteractor;
 
     private bool _selected = false;
-    protected bool _ready = false;
-    
-
-    protected virtual void Start()
-    {
-        if(readyOnStart)
-        {
-            _ready = true;
-        }
-    }
-
     // Returns whether the object is selected
     public bool Selected
     {
@@ -33,35 +34,14 @@ public class InteractableObject : MonoBehaviour
         private set { }
     }
 
-    // Selects the object
-    public void Select(WorldInteractor worldInteractor)
-    {
-        _selected = true;
-        _worldInteractor = worldInteractor;
-    }
+    protected bool _ready = false;
 
-    // Deselects the object
-    public void Deselect()
-    {
-        _selected = false;
-        _worldInteractor = null;
-    }
-    
-    public virtual bool ReadyForInteraction()
-    {
-        return _ready;
-    }
 
-    // Interacts with the object
-    public virtual void Interact()
+    protected virtual void Start()
     {
-        if (_ready)
+        if (readyOnStart)
         {
-            Consume();
-        }
-        else
-        {
-            // Give some negative feedback
+            _ready = true;
         }
     }
 
@@ -74,11 +54,63 @@ public class InteractableObject : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Selects the object and assigns the WorldInteractor that selected it.
+    /// </summary>
+    /// <param name="worldInteractor">The WorldInteractor that selected the object.</param>
+    public void Select(WorldInteractor worldInteractor)
+    {
+        _selected = true;
+        _worldInteractor = worldInteractor;
+    }
+
+    /// <summary>
+    /// Deselects the object and removes the reference to the WorldInteractor that selected it.
+    /// </summary>
+    public void Deselect()
+    {
+        _selected = false;
+        _worldInteractor = null;
+    }
+
+    /// <summary>
+    /// Determines whether the object is ready for interaction.
+    /// Override this method to customize the readiness check.
+    /// </summary>
+    public virtual bool ReadyForInteraction()
+    {
+        return _ready;
+    }
+
+    /// <summary>
+    /// Defines the interaction behavior of the object.
+    /// Override this method to customize the interaction behavior.
+    /// </summary>
+    public virtual void Interact()
+    {
+        if (_ready)
+        {
+            Consume();
+        }
+        else
+        {
+            // Give some negative feedback
+        }
+    }
+
+    /// <summary>
+    /// Defines the behavior when the object is consumed.
+    /// Override this method to customize the consumption behavior.
+    /// </summary>
     protected virtual void Consume()
     {
         _ready = false;
     }
 
+    /// <summary>
+    /// Defines the behavior when the object is made ready for interaction.
+    /// Override this method to customize the readiness behavior.
+    /// </summary>
     protected virtual void MakeReady()
     {
         _ready = true;
