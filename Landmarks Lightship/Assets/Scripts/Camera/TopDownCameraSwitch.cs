d5b901e52919709963f663c2d539e94d4674ef7d
elsilverberg <elsilverberg@sbcglobal.net>
2023-12-02 13:24:07 -0500

    Camera switches from orbit to top down correctly. (Without fog issues)

--- Landmarks Lightship/Assets/Scripts/Camera/TopDownCameraSwitch.cs
+++ Landmarks Lightship/Assets/Scripts/Camera/TopDownCameraSwitch.cs
@@ -7,83 +7,134 @@ using UnityEngine;
 public class TopDownCameraSwitch : MonoBehaviour
 {
 
+
     //Set Orbit camera gameobjects in inspector
+
+
+    //This keeps track of the Orbit Camera prefab, both the camera and the empty object where the OrbitCameraController code is located. 
+
     public GameObject OrbitCameraObject;
     public GameObject OrbitCamera;
 
 
-    //Track camera rotation to prevent sticking
-    public float camerarotationtracker = 45f; 
 
 
-    //Set Orthographic camera gameobject and camera in inspector
+
+
+
+
+    //Bool that is used to prevent sticking
+
+    //the tracker is used to track the camera's rotation for the sake of specific parts of the code. 
+    public float camerarotationtracker = .45f;
+
+
+    //This keeps track of the Orthographic camera prefab, but the second thing is the Camera, not the camera's gameobject, since we're using a value of the camera instead of the transform. 
     public GameObject OrthographicCameraObject;
     public Camera OrthographicCamera;
 
-    //Bool that is used to prevent sticking
+
+
+    //Tracks whether or not the camera is allowed to swap. Very important for causing things to not break. 
+
     public bool readySwap = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Makes the tracker track the camera zoom. 
-        camerarotationtracker = OrbitCamera.transform.rotation.x;
+        camerarotationtracker = OrbitCamera.transform.rotation.eulerAngles.x;
+
+
+        print(camerarotationtracker);
 
         //Re-enable swapping if zoomed in a tiny bit beyond the orbit max
-        if (camerarotationtracker <= .49f && readySwap == false)
+        if (OrbitCamera.transform.rotation.eulerAngles.x <= 59.9 && readySwap == false)
         {
             readySwap = true;
         }
 
-       
+
+
+
+
+
+
+
 
         //Swaps from orbit to orthographic if swapping is enabled and camera is active at max zoom distance. 
-        if (OrbitCamera.transform.rotation.x == .5f && OrbitCameraObject.activeSelf && readySwap == true)
+        if (OrbitCamera.transform.rotation.eulerAngles.x >= 60 && OrbitCameraObject.activeSelf && readySwap == true)
         {
 
+
+            //Disables swapping to Orthographic again until reset with the above code
             readySwap = false;
-   
+
+            //Disables the Orbit Camera
+
             OrbitCameraObject.SetActive(false);
+
+            RenderSettings.fog = false;
+
+            //Sets the orthographic camera to its starting values and makes it active.
             OrthographicCamera.orthographicSize = 166;
             OrthographicCameraObject.SetActive(true);
+
         }
 
 
-        //Swaps back to orbit if at minimum designated zoom for Orthographic. 
+
+
+
+
+
+
+        //When the camera is zoomed in enough, and the orthographic camera is active, swaps back to orbit
         if (OrthographicCamera.orthographicSize <= 165 && OrthographicCameraObject.activeSelf)
         {
 
+            //Same process as above, but reverse.
+            //Disables and resets Orthographic to something that cannot cause issues in the future
             OrthographicCamera.orthographicSize = 170;
             OrthographicCameraObject.SetActive(false);
 
+            //And activates the Orbit Camera 
             OrbitCameraObject.SetActive(true);
-            
-            
+
+            RenderSettings.fog = true;
+
+
         }
     }
-
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{ readySwap = true;
-         //   if (OrbitCameraObject.activeInHierarchy == true)
-          //  {
-          //      OrbitCameraObject.SetActive(false);
-          //      OrthographicCameraObject.SetActive(true);
-          //  }
-
-          //  else if (OrthographicCameraObject.activeInHierarchy == true)
-         //   {
-           //     OrbitCameraObject.SetActive(true);
-          //      OrthographicCameraObject.SetActive(false);
-          //  }
-        }
+}
 
 
 
-    
+
+
+//This is code that can be used to manually swap cameras, mostly used for testing.
+//if (Input.GetKeyDown(KeyCode.Space))
+//{ readySwap = true;
+//   if (OrbitCameraObject.activeInHierarchy == true)
+//  {
+//      OrbitCameraObject.SetActive(false);
+//      OrthographicCameraObject.SetActive(true);
+//  }
+
+//  else if (OrthographicCameraObject.activeInHierarchy == true)
+//   {
+//     OrbitCameraObject.SetActive(true);
+//      OrthographicCameraObject.SetActive(false);
+//  }
+//}
+
+
+
+
 
