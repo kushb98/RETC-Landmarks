4cf8d1c3c40f89f28db288b17f43d941ac01823f
Txhowar <77594787+Txhowar@users.noreply.github.com>
2024-04-05 17:01:20 -0500

    Inventory & Shop System
    +Players can buy items from the shop and lose coins
    +Added delete and clear inventory buttons
    +Removed Unity Version Control

--- Landmarks Lightship/Assets/Scripts/Interactable/Item/InventoryManager.cs
+++ Landmarks Lightship/Assets/Scripts/Interactable/Item/InventoryManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 using UnityEngine.Rendering.Universal;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using JetBrains.Annotations;
 
 public class InventoryManager : MonoBehaviour, IDataPersistence
 {
@@ -13,6 +14,10 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
 
     [SerializeField]
     public List<Item> Items = new List<Item>();
+    //public List<Item> ShopItems = new List<Item>();
+
+    public InventoryItemController[] InventoryItems;
+    public InventoryItemController[] BoughtItems;
 
     public GameObject RoamlingMenu;
     public Transform ItemContent;
@@ -20,11 +25,14 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
     public RoamlingController roamlingController;
     public GameObject Inventory;
 
+    public GameObject ItemInventory;
+    public Transform BoughtContent;
+
     public Toggle ReleaseRoamlings;
 
-    public InventoryItemController[] InventoryItems;
 
-    
+
+
     [SerializeField] private Item itemDataSO;
     private void Awake()
     {
@@ -33,14 +41,14 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
         else
             Debug.LogError("Multiple Inventory Managers In Scene");
 
-       
+
     }
 
     private void Start()
     {
         ListItems(); // Initially list items
-       
-        
+
+
     }
 
     public void LoadData(GameData data)
@@ -56,20 +64,20 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
          itemDataSO.icon = data.itemData.icon;
          itemDataSO.value = data.itemData.value;
         */
-        Items = data.itemData.Items;
+       Items = data.itemData.Items;
 
     }
 
     public void SaveData(ref GameData data)
     {
-       /* data.itemData.Hunger = itemDataSO.Hunger;
-        data.itemData.Happiness = itemDataSO.Happiness;
-        data.itemData.maxHunger = itemDataSO.maxHunger;
-        data.itemData.maxHappiness = itemDataSO.maxHappiness;
-        data.itemData.itemName = itemDataSO.itemName;
-        data.itemData.icon = itemDataSO.icon;
-        data.itemData.value = itemDataSO.value;
-       */
+        /* data.itemData.Hunger = itemDataSO.Hunger;
+         data.itemData.Happiness = itemDataSO.Happiness;
+         data.itemData.maxHunger = itemDataSO.maxHunger;
+         data.itemData.maxHappiness = itemDataSO.maxHappiness;
+         data.itemData.itemName = itemDataSO.itemName;
+         data.itemData.icon = itemDataSO.icon;
+         data.itemData.value = itemDataSO.value;
+        */
         data.itemData.Items = Items;
 
 
@@ -89,6 +97,14 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
         List<Item> newItems = Items;
         newItems.Remove(item);
         Items = newItems;
+
+
+    }
+
+    public void ClearInventory()
+    {
+        Items.Clear();
+        ListItems();
     }
 
     public void ListItems()
@@ -98,22 +114,44 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
             Destroy(item.gameObject);
         }
 
+       foreach (Transform item in BoughtContent)
+        {
+            Destroy(item.gameObject);
+        }
+      
         foreach (var item in Items)
         {
-            GameObject obj = Instantiate(InventoryItem, ItemContent);
-            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
-            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
-            var removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
-
-            itemName.text = item.itemName;
-            itemIcon.sprite = item.icon;
-
-            // Add an onClick event to each instantiated item
-           obj.GetComponent<Button>().onClick.AddListener(() => OnItemClick(item));
-
-            if (ReleaseRoamlings.isOn)
+            if (item.value > 0)
             {
-                removeButton.gameObject.SetActive(true);
+                GameObject obj = Instantiate(InventoryItem, ItemContent);
+
+
+                var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+                var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+                var removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
+
+                itemName.text = item.itemName;
+                itemIcon.sprite = item.icon;
+
+                obj.GetComponent<Button>().onClick.AddListener(() => OnItemClick(item));
+
+                if (ReleaseRoamlings.isOn)
+                {
+                    removeButton.gameObject.SetActive(true);
+                }
+
+            }
+
+            // if the item value is 0, do the same for BoughtContent
+            if (item.value == 0)
+            {
+                GameObject obj2 = Instantiate(InventoryItem, BoughtContent);
+                var itemName2 = obj2.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+                var itemIcon2 = obj2.transform.Find("ItemIcon").GetComponent<Image>();
+                var removeButton2 = obj2.transform.Find("RemoveButton").GetComponent<Button>();
+
+                itemName2.text = item.itemName;
+                itemIcon2.sprite = item.icon;
             }
         }
 
@@ -158,13 +196,43 @@ public class InventoryManager : MonoBehaviour, IDataPersistence
 
     public void SetInventoryItems()
     {
+        //set the inventory items to the number in both item content and bought content
         InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+        BoughtItems = BoughtContent.GetComponentsInChildren<InventoryItemController>();
 
-        for (int i = 0; i < Items.Count; i++)
+        // for (int i = 0; i < Items.Count - BoughtContent.childCount; i++)
+        for (int i = 0; i < Items.Count - BoughtContent.childCount; i++)
         {
             InventoryItems[i].AddItem(Items[i]);
         }
+       
+        /*if (Items.Count > 0)
+        {
+            for (int i = 0; i < Items.Count - BoughtContent.childCount; i++)
+            {
+                InventoryItems[i].AddItem(Items[i]);
+            }
+        }
+      /*  else
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                BoughtItems[i].AddItem(Items[i]);
+            }
+        }
+     /* for (int i = 0; i < Items.Count; i++)
+        {
+            BoughtItems[i].AddItem(Items[i]);
+        }
+     */   
+
     }
+
+    private void OnApplicationQuit()
+    {
+        ListItems();
+    }
+
 
 }
 
